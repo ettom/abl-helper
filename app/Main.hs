3cd62c51ce2ea9c900f7dc53274a8ab4d038dcca
ettom <36895504+ettom@users.noreply.github.com>
2021-01-04 18:21:22 +0200

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
 module Main where
 
 import Lib
 
+import Control.Monad      (join)
+import Data.Text.IO       (putStrLn, readFile)
+import Flow
+import Numeric
 import System.Environment
 import System.Exit
-
-import Flow
-
 import Text.Layout.Table
 
-import Data.List (elemIndices)
-import Numeric
-
 import qualified Data.Text as T
+import           Prelude   hiding (putStrLn, readFile)
 
-main = getArgs >>= parse >>= putStrLn . doWork
+shortPrecision = 3
+longPrecision = 6
+possibleGridSizes = [2..10]
+
+main :: IO ()
+main = getArgs >>= parse >>= putStrLn . makeOutput
     where
       parse [] = putStrLn "Give a text file containing the measurements as an input!" >> exitWith (ExitFailure 1)
       parse (x:_) = readFile x
-
-      possibleGridSizes = [2..10]
 
       tryGetGrids :: [Int] -> T.Text -> Maybe [Grid]
       tryGetGrids [] _ = Nothing
@@ -28,43 +32,64 @@ main = getArgs >>= parse >>= putStrLn . doWork
           Just grids -> if not (null grids) then Just grids else Nothing
           Nothing    -> tryGetGrids xs fileContents
 
-      doWork :: String -> String
-      doWork fileContents =
-        case tryGetGrids possibleGridSizes (T.pack fileContents) of
+      highlight :: T.Text -> T.Text -> T.Text -> T.Text
+      highlight color target = T.replace (" " <> target <> " ") (color <> " " <> target <> " " <> revert)
+        where
+          revert = "\ESC[0m"
+
+      makeOutput :: T.Text -> T.Text
+      makeOutput fileContents =
+        case tryGetGrids possibleGridSizes fileContents of
           Nothing -> "Could not parse grids!"
           Just grids ->
-            let rowgroups = unifyGrids grids |> getCalculation |> map (map formatCalculation) |> map (colsAllG center)
+            let calculations = unifyGrids grids |> map (map mkCalculation)
+                rowgroups = map (colsAllG center . map (map T.unpack . formatCalculation)) calculations
                 table = tableString (repeat def) unicodeS def rowgroups
+                stddevs = map _stddev $ join calculations
+                ranges = map _range $ join calculations
+                avg_stddev = fixPrecision longPrecision $ avg stddevs
+                max_stddev = fixPrecision longPrecision $ maximum stddevs
+                min_stddev = fixPrecision longPrecision $ minimum stddevs
+                avg_range = fixPrecision shortPrecision $ avg ranges
+                max_range = fixPrecision shortPrecision $ maximum ranges
+                min_range = fixPrecision shortPrecision $ minimum ranges
+                red = "\ESC[31m"
+                green = "\ESC[32m"
+                bright_red = "\ESC[91m"
+                bright_green = "\ESC[92m"
+                alignment = T.replicate (longPrecision - shortPrecision) " "
 
-            in "Found " ++ show (length grids) ++ " measurement grids...\n" ++ table
+            in highlight bright_red max_stddev $
+               highlight bright_green min_stddev $
+               highlight green min_range $
+               highlight red max_range $
+                  "Found " <> T.pack (show $ length grids) <> " measurement grids...\n"
+               <> "SD    - Mean: " <> avg_stddev <> " Max: " <> max_stddev <> " Min: " <> min_stddev <> " \n"
+               <> "Range - Mean: " <> avg_range <> alignment <> " Max: " <> max_range <> alignment <> " Min: " <> min_range <> " \n"
+               <> T.pack table
 
-
-formatCalculation :: Calculation -> [String]
+formatCalculation :: Calculation -> [T.Text]
 formatCalculation c =
   let tmp = [
-              "Mean: "  ++ (fixPrecision longPrecision  (_mean c) |> addPlus)
-            , "Min: "   ++ (fixPrecision shortPrecision (_min c)  |> addPlus)
-            , "Max: "   ++ (fixPrecision shortPrecision (_max c)  |> addPlus)
-            , "Range: " ++  fixPrecision shortPrecision (_range c)
-            , "SD: "    ++  fixPrecision longPrecision  (_stddev c)
+              "Mean: "  <> (fixPrecision longPrecision  (_mean c) |> addPlus)
+            , "Min: "   <> (fixPrecision shortPrecision (_min c)  |> addPlus)
+            , "Max: "   <> (fixPrecision shortPrecision (_max c)  |> addPlus)
+            , "Range: " <>  fixPrecision shortPrecision (_range c)
+            , "SD: "    <>  fixPrecision longPrecision  (_stddev c)
             ]
-      longest = maximum $ map length tmp
-      in map (padLine longest) tmp |> justify longest
+      longest = maximum $ map T.length tmp
+      in map (padLine longest) tmp |> map T.unpack |> justify longest |> map T.pack
 
     where
-      shortPrecision = 3
-      longPrecision = 6
-
-      padLine :: Int -> String -> String
+      padLine :: Int -> T.Text -> T.Text
       padLine resultLength line
-        | length line < resultLength = padLine resultLength insertSpace
-        | otherwise    = line
-          where firstSpace = elemIndices ' ' line |> head
-                insertSpace = let (ys,zs) = splitAt firstSpace line in ys ++ " " ++ zs
+        | T.length line < resultLength = padLine resultLength insertSpace
+        | otherwise                    = line
+          where
+            insertSpace = let (ys,zs) = T.breakOn " " line in ys <> " " <> zs
 
+      addPlus :: T.Text -> T.Text
+      addPlus x = if T.head x /= '-' then T.cons '+' x else x
 
-      fixPrecision :: Int -> Double -> String
-      fixPrecision numOfDecimals x = showFFloat (Just numOfDecimals) x ""
-
-      addPlus :: String -> String
-      addPlus x = if head x /= '-' then '+' : x else x
+fixPrecision :: Int -> Double -> T.Text
+fixPrecision numOfDecimals x = T.pack $ showFFloat (Just numOfDecimals) x ""
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,9 +10,7 @@ import Text.Read
 import qualified Data.Text as T
 
 type Grid = [[Double]]
-type GroupedGrid = [Double]
-
-
+type GridsAggregated = [[[Double]]] -- A matrix of grid values (grouped by coordinates)
 data Calculation = Calculation {
                        _mean   :: Double
                      , _min    :: Double
@@ -21,32 +19,14 @@ data Calculation = Calculation {
                      , _stddev :: Double
                      }
 
-
 mkCalculation :: [Double] -> Calculation
 mkCalculation xs = Calculation {
-                                _mean = mean',
-                                _min = min',
-                                _max = max',
-                                _range = range',
-                                _stddev = stddev'
+                                 _mean = avg xs
+                               , _min = minimum xs
+                               , _max = maximum xs
+                               , _range = abs $ minimum xs - maximum xs
+                               , _stddev = map ((^(2 :: Integer)) . (-) (avg xs)) xs |> avg |> sqrt
                                }
-  where
-    mean' = avg xs
-    min' = minimum xs
-    max' = maximum xs
-    range' = abs $ min' - max'
-    stddev' = map ((^2) . (-) mean') xs |> avg |> sqrt
-
-    avg :: (Fractional a) => [a] -> a
-    avg x = total / fromIntegral counter
-      where
-        (counter, total) = runningSum x
-        runningSum :: (Num a) => [a] -> (Int, a)
-        runningSum = foldl (\acc x -> bimap (+ 1) (+ x) acc) (0,0)
-
-
-getCalculation :: [[GroupedGrid]] -> [[Calculation]]
-getCalculation = map (map mkCalculation)
 
 getGrids :: Int -> T.Text -> Maybe [Grid]
 getGrids gridSize x = let cleaned = T.lines x |> map cleanUp |> filter (/= T.empty)
@@ -58,21 +38,29 @@ getGrids gridSize x = let cleaned = T.lines x |> map cleanUp |> filter (/= T.emp
     validateChunks = all (\z -> length z == gridSize)
 
     cleanUp :: T.Text -> T.Text
-    cleanUp x = T.break (\z -> z == '-' || z == '+') x |> snd |> T.strip |> T.replace "+" ""
+    cleanUp text = T.break (\z -> z == '-' || z == '+') text |> snd |> T.strip |> T.replace "+" ""
 
     toGrid :: [T.Text] -> Maybe Grid
     toGrid = mapM toRow
       where
         toRow :: T.Text -> Maybe [Double]
-        toRow x
-          | length cells == gridSize = T.words x |> traverse (\z -> readMaybe (T.unpack z) :: Maybe Double)
+        toRow text
+          | length cells == gridSize = T.words text |> traverse (\z -> readMaybe (T.unpack z) :: Maybe Double)
           | otherwise                = Nothing
-          where cells = T.words x
+          where cells = T.words text
 
 
-unifyGrids :: [Grid] -> [[GroupedGrid]]
+unifyGrids :: [Grid] -> GridsAggregated
+unifyGrids [] = error "Logic error"
 unifyGrids [x] = map (map (: [])) x
 unifyGrids (x : xs) = gridZip (unifyGrids xs) x
   where
-    gridZip :: [[GroupedGrid]] -> Grid -> [[GroupedGrid]]
+    gridZip :: GridsAggregated -> Grid -> GridsAggregated
     gridZip = zipWith (zipWith (flip (:)))
+
+avg :: (Fractional a) => [a] -> a
+avg x = total / fromIntegral counter
+  where
+    (counter, total) = runningSum x
+    runningSum :: (Num a) => [a] -> (Int, a)
+    runningSum = foldl (\acc y -> bimap (+ 1) (+ y) acc) (0,0)
